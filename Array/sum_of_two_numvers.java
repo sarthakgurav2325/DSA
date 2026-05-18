c708130ee06455816dfa0d160b8b6c3ca8027c5d
avinashbankar007 <avinashbankar97@gmail.com>
2026-05-18 11:02:17 +0530

    18-05-2026

---
+++ Array/sum_of_two_numvers.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            int complement = target - nums[i];
+            
+            // If complement exists, return indices
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            
+            // Store current number with index
+            map.put(nums[i], i);
+        }
+        
+        return new int[] {};
+    }
+}
